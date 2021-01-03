0da470922952c6b8e73a4491b3126d71fa4b0088
Adame <Adame.haouari@gmail.com>
2021-01-03 23:51:50 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let trois = ["tokyo", "paris", "ny"]
+console.log(trois)
+
+
+trois.push("Adame")
+console.log(trois)
+
+
+trois.splice(0, 1, "tanger")
+console.log(trois)
+
+trois.shift("test")
+console.log(trois)
